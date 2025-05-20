6e48d31cd2ce2f9d04155ed7223b9e2798d59e65
Jonathan Cojita <jcoji022@uottawa.ca>
2025-05-20 11:57:36 -0400

    db setup

---
+++ ServerSide/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
+
+class Accounts(db.Model):
+    userId = db.Column(db.String(30), primary_key=True)
+    password = db.Column(db.String(30), nullable=False)
+    
+
+    def __repr__(self):
+        return '<Account %r>' % self.userId
+
+class Cards(db.Model):
+    cardId = db.Column(db.Integer, primary_key=True)
+    userId = db.Column(db.String(30), foreighn_key=True)
+    vocabNL = db.Column(db.String(100), nullable=False)
+    vocabTL = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(500))
+    dateCreated = db.Column(db.DateTime, default=datetime.utcnow)
+
+    
+    def __repr__(self):
+        return '<Card %r>' % self.cardID
+    
+with app.app_context():
+        db.create_all()
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
